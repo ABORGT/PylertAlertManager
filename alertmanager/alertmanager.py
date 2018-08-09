1811c51d508b9692e8db96f27881ac5da72c06eb
Justin Palmer <jpavlav@gmail.com>
2018-08-09 07:01:16 -0500

    Removed some debug code.

--- alertmanager/alertmanager.py
+++ alertmanager/alertmanager.py
@@ -6,22 +6,7 @@ import logging
 import json
 import copy
 import maya
-import logging
 from box import Box, BoxKeyError
-
-try:
-    import http.client as http_client
-except ImportError:
-    # Python 2
-    import httplib as http_client
-http_client.HTTPConnection.debuglevel = 1
-
-# You must initialize logging, otherwise you'll not see debug output.
-logging.basicConfig()
-logging.getLogger().setLevel(logging.DEBUG)
-requests_log = logging.getLogger("requests.packages.urllib3")
-requests_log.setLevel(logging.DEBUG)
-requests_log.propagate = True
 
 
 class AlertManager(object):
